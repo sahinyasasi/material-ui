13295b0390b02ad47774b9bbb88e1e859f983571
sahinyasasi <sahinya.slc@gmail.com>
2021-02-01 10:28:49 +0530

    adding darktheme to site

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
+import React, { useState } from "react";
+import { Grid, Typography, Switch, Button, Paper } from "@material-ui/core";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 function App() {
-  return <div className='App'></div>;
+  const [darkMode, setDarkMode] = useState(false);
+  const theme = createMuiTheme({
+    palette: {
+      type: darkMode ? "dark" : "light",
+    },
+  });
+  return (
+    <ThemeProvider theme={theme}>
+      <Paper style={{ height: "100vh" }}>
+        <Grid container direction='column'>
+          <Typography variant='h1'>This is my App!</Typography>
+          <Button variant='contained' color='primary'>
+            This is a Button
+          </Button>
+          <Button variant='contained' color='secondary'>
+            This is another Button
+          </Button>
+          <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
+        </Grid>
+      </Paper>
+    </ThemeProvider>
+  );
 }
-
 export default App;
